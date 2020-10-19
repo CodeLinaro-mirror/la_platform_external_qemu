9216ab2c1a999b41d69e4afffd792ffb026a9ca8
Shu-Chun Weng <scw@google.com>
2020-10-19 15:28:38 -0700

    util: Only prepend QEMU_LD_PREFIX for libraries
    This is a port of cl/279397522
    
    The upstream path rewrite is too aggressive prepending the prefix for all file
    access as long as the modified path exists. It breaks programs that open "/" and
    walk the directory structure themselves -- g_build_filename(QEMU_LD_PREFIX, "/")
    is equivalent to QEMU_LD_PREFIX, and the folder exists, making any open("/")
    call to open the QEMU_LD_PREFIX folder, not the real root folder.
    
    Tested: https://test.corp.google.com/ui#id=OCL:336333313:BASE:336333872:1602268745648:d361c231
    PiperOrigin-RevId: 337946933
    
    util: Relax path-rewriting to also rewrite paths that may contain libraries.
    
    As seen in http://b/171580661. `ld.so` may only search for libraries in a
    given directory if that directory exists on the system. As of
    http://cl/337946933 only path names ending with `.so(.\d)*` are re-written
    with the QEMU_LD_PREFIX. This prevented `ld.so` from discovering some critical
    libraries used in the chromiumos sysroot.
    
    This change re-adds path rewriting based on QEMU_LD_PREFIX for any directories
    that commonly contain dyanmic libraries. E.g., `/lib`, `/usr/lib` etc.
    
    Tested: TGP: https://test.corp.google.com/OCL:338732435:BASE:338732752:1603484255249:70bba06e
      blaze test --config=chromiumos_arm --compilation_mode=opt --copt=-UNDEBUG //third_party/iree/iree/base:ref_ptr_test
        http://sponge2/4ffe8474-057f-41f8-bc98-52d0390edc5d
    
    GitWatcher: ignore
    Google-Bug-Id: 262974133
    PiperOrigin-RevId: 338738113
    Signed-off-by: Shu-Chun Weng <scw@google.com>
    Change-Id: I5e2f1c346a14a1eccb17b333ed916c9dced9b303

--- util/path.c
+++ util/path.c
@@ -6,6 +6,7 @@
 #include "qemu/osdep.h"
 #include <sys/param.h>
 #include <dirent.h>
+#include <regex.h>
 #include "qemu/cutils.h"
 #include "qemu/path.h"
 #include "qemu/thread.h"
@@ -13,9 +14,11 @@
 static const char *base;
 static GHashTable *hash;
 static QemuMutex lock;
+static regex_t shared_library_regex;
 
 void init_paths(const char *prefix)
 {
+    int err;
     if (prefix[0] == '\0' || !strcmp(prefix, "/")) {
         return;
     }
@@ -30,6 +33,38 @@ void init_paths(const char *prefix)
 
     hash = g_hash_table_new(g_str_hash, g_str_equal);
     qemu_mutex_init(&lock);
+
+    err = regcomp(&shared_library_regex, "\\.so(\\.[[:digit:]]+)*$",
+                  REG_EXTENDED | REG_NOSUB);
+    if (err != 0) {
+        fprintf(stderr, "regcomp returns error code %d\n", err);
+        exit(EXIT_FAILURE);
+    }
+}
+
+/*
+ * Checks if a filename indicates that it's a shared object file by matching the
+ * file name against /\.so(\.[0-9]+)*$/.
+ */
+static bool is_filename_shared_object(const char *name)
+{
+    return regexec(&shared_library_regex, name, 0, NULL, 0) == 0;
+}
+
+/*
+ * Checks if a filename indicates that it may contain shared object files, and
+ * so therefore should be wrapped by QEMU_LD_PREFIX.
+ */
+static bool is_filename_libdir(const char *name)
+{
+    return (
+        g_str_has_prefix(name, "/lib")
+        || g_str_has_prefix(name, "/usr/lib")
+        || g_str_has_prefix(name, "/usr/local/lib")
+        || g_str_has_prefix(name, "/lib64")
+        || g_str_has_prefix(name, "/usr/lib64")
+        || g_str_has_prefix(name, "/usr/local/lib64")
+    );
 }
 
 /* Look for path in emulation dir, otherwise return name. */
@@ -48,7 +83,8 @@ const char *path(const char *name)
     /* Have we looked up this file before?  */
     if (g_hash_table_lookup_extended(hash, name, &key, &value)) {
         ret = value ? value : name;
-    } else {
+    } else if (is_filename_shared_object(name) || is_filename_libdir(name)) {
+        /* GOOGLE CHANGE only prepend QEMU_LD_PREFIX for libraries */
         char *save = g_strdup(name);
         char *full = g_build_filename(base, name, NULL);
 
@@ -63,6 +99,9 @@ const char *path(const char *name)
             g_hash_table_insert(hash, save, NULL);
             ret = name;
         }
+    } else {
+        g_hash_table_insert(hash, g_strdup(name), NULL);
+        ret = name;
     }
 
     qemu_mutex_unlock(&lock);
