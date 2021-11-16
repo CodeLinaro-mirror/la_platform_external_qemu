2268f8a98111aaa77d2b3303186ee187f4443a00
Titus Rwantare <titusr@google.com>
2021-11-16 17:53:33 +0000

    hw/peci: add PECI support for NPCM7xx BMCs
    This allows BMC firmware for npcm7xx BMCs to talk to a PECI client
    in qemu.
    
    Titus Rwantare (4):
          hw/peci: add PECI support for NPCM7xx BMCs
          hw/peci: stop asserting STS_ABRT_ERR on failure
          hw/peci: set error bit on ping to non-present socket
          hw/peci: clear CRC ERR bit after it's read
    
    GitWatcher: ignore
    Google-Bug-Id: 262933331
    Google-Bug-Id: 262946253
    Google-Bug-Id: 170252997
    Google-Bug-Id: 235123328
    Google-Bug-Id: 262945663
    Google-Bug-Id: 238814803
    Signed-off-by: Titus Rwantare <titusr@google.com>
    Change-Id: I9e446025aa4d96878a9a22375be68a500473215f

---
+++ include/hw/peci/npcm7xx_peci.h
@@ -0,0 +1,37 @@
+/*
+ * Nuvoton NPCM7xx PECI Module
+ *
+ * Copyright 2021 Google LLC
+ *
+ * SPDX-License-Identifier: GPL-2.0-or-later
+ */
+
+#ifndef NPCM7XX_PECI_H
+#define NPCM7XX_PECI_H
+
+#include "system/memory.h"
+#include "hw/core/irq.h"
+#include "hw/peci/peci.h"
+#include "hw/core/sysbus.h"
+
+typedef struct NPCM7xxPECIState {
+    SysBusDevice parent;
+
+    MemoryRegion iomem;
+
+    PECIBus      *bus;
+    qemu_irq      irq;
+
+    PECICmd       pcmd;
+
+    /* Registers */
+    uint8_t       status;
+    uint8_t       ctl2;
+    uint8_t       pdrr;
+} NPCM7xxPECIState;
+
+#define TYPE_NPCM7XX_PECI "npcm7xx-peci"
+#define NPCM7XX_PECI(obj)                                               \
+    OBJECT_CHECK(NPCM7xxPECIState, (obj), TYPE_NPCM7XX_PECI)
+
+#endif /* NPCM7XX_PECI_H */
