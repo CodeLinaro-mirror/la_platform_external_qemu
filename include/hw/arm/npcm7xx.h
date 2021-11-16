2467a5c70f42922929a27993b049bf968f5e18f5
Titus Rwantare <titusr@google.com>
2021-11-16 17:53:33 +0000

    hw/peci: add PECI support for NPCM7xx BMCs
    This allows BMC firmware for npcm7xx BMCs to talk to a PECI client
    in qemu.
    
    Google-Rebase-Count: 6
    Google-Bug-Id: 262933331
    Google-Bug-Id: 170252997
    Signed-off-by: Titus Rwantare <titusr@google.com>
    Change-Id: I9e446025aa4d96878a9a22375be68a500473215f

--- include/hw/arm/npcm7xx.h
+++ include/hw/arm/npcm7xx.h
@@ -31,6 +31,7 @@
 #include "hw/net/npcm7xx_emc.h"
 #include "hw/net/npcm_gmac.h"
 #include "hw/nvram/npcm7xx_otp.h"
+#include "hw/peci/npcm7xx_peci.h"
 #include "hw/timer/npcm7xx_timer.h"
 #include "hw/ssi/npcm7xx_fiu.h"
 #include "hw/ssi/npcm_pspi.h"
@@ -78,7 +79,7 @@ typedef struct NPCM7xxMachineClass {
 #define NPCM7XX_MACHINE_GET_CLASS(obj)                                  \
     OBJECT_GET_CLASS(NPCM7xxMachineClass, (obj), TYPE_NPCM7XX_MACHINE)
 
-struct NPCM7xxState {
+typedef struct NPCM7xxState {
     DeviceState         parent;
 
     ARMCPU              cpu[NPCM7XX_MAX_NUM_CPUS];
@@ -108,7 +109,8 @@ struct NPCM7xxState {
     NPCMGMACState       gmac[2];
     NPCM7xxSDHCIState   mmc;
     NPCMPSPIState       pspi[2];
-};
+    NPCM7xxPECIState    peci;
+} NPCM7xxState;
 
 #define TYPE_NPCM7XX    "npcm7xx"
 OBJECT_DECLARE_TYPE(NPCM7xxState, NPCM7xxClass, NPCM7XX)
