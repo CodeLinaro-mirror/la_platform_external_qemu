ba7d68fd29a3d9cd64b9d400ae8cb1ac16f65196
Hao Wu <wuhaotsh@google.com>
2022-06-30 10:02:38 -0700

    hw/i2c: Add a CMIS module
    Common Management Interface Specification (CMIS) is a protocol
    used by a bunch of modules including QSFP, OSFP, COBO, etc.
    The specification can be found at
    http://www.qsfp-dd.com/wp-content/uploads/2021/05/CMIS5p0.pdf
    
    GitWatcher: ignore
    Google-Bug-Id: 262946808
    Signed-off-by: Hao Wu <wuhaotsh@google.com>
    Google-Bug-Id: 237000544
    Change-Id: Iab943bb07f1ed609a5605fc8a7f4c83fb9510b06

---
+++ include/hw/i2c/cmis.h
@@ -0,0 +1,50 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+/*
+ * Common Management Interface Specification (CMIS) Module
+ *
+ * This module describes a CMIS device. The specs can be found at
+ * http://www.qsfp-dd.com/wp-content/uploads/2019/05/QSFP-DD-CMIS-rev4p0.pdf
+ *
+ * Copyright 2022 Google LLC
+ */
+#ifndef CMIS_H
+#define CMIS_H
+
+#include "hw/i2c/i2c.h"
+#include "qom/object.h"
+
+#define CMIS_PAGE_SIZE 0x80
+
+typedef enum CMISMode {
+    CMIS_IDLE,
+    CMIS_WRITE_ADDR,
+    CMIS_WRITE_DATA,
+    CMIS_READ_DATA,
+    CMIS_DONE,
+} CMISMode;
+
+typedef struct CMISDevice {
+    /* CMIS device is on top of I2C protocol. */
+    I2CSlave parent;
+
+    CMISMode mode;
+    uint8_t current_addr;
+    uint8_t bank_addr;
+    uint8_t page_addr;
+} CMISDevice;
+
+typedef struct CMISClass {
+    I2CSlaveClass parent_class;
+
+    uint8_t (*read_lower_page)(CMISDevice *d, uint8_t addr);
+    int (*write_lower_page)(CMISDevice *d, uint8_t addr, uint8_t data);
+    uint8_t (*read_upper_page)(CMISDevice *d, uint8_t bank_addr,
+                               uint8_t page_addr, uint8_t addr);
+    int (*write_upper_page)(CMISDevice *d, uint8_t bank_addr, uint8_t page_addr,
+                             uint8_t addr, uint8_t data);
+} CMICClass;
+
+#define TYPE_CMIS_DEVICE "cmis-device"
+OBJECT_DECLARE_TYPE(CMISDevice, CMISClass, CMIS_DEVICE)
+
+#endif /* CMIS_H */
