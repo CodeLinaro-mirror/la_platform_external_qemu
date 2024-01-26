af1f009a4356e1c968fa8f0a7b9b6957af2d2fb5
Felix Wu <flwu@google.com>
2024-01-26 01:12:54 +0000

    net/checksum.c: fix the unaligned memory access issue.
    Get rid of any direct member accessing as they could be unaligned.
    
    Change-Id: I83908ec3545fe3aed72d748a47d0c5d6da47dc28
    Signed-off-by: Felix Wu <flwu@google.com>

--- net/checksum.c
+++ net/checksum.c
@@ -14,10 +14,12 @@
  *  You should have received a copy of the GNU General Public License
  *  along with this program; if not, see <http://www.gnu.org/licenses/>.
  */
+#include <stddef.h>
 
 #include "qemu/osdep.h"
 #include "net/checksum.h"
 #include "net/eth.h"
+#include "qemu/log.h"
 
 uint32_t net_checksum_add_cont(int len, uint8_t *buf, int seq)
 {
@@ -47,26 +49,70 @@ uint16_t net_checksum_finish(uint32_t sum)
 }
 
 uint16_t net_checksum_tcpudp(uint16_t length, uint16_t proto,
-                             uint8_t *addrs, uint8_t *buf)
+                             uint8_t *addrs, uint8_t *buf,
+                             uint8_t addr_len)
 {
     uint32_t sum = 0;
 
-    sum += net_checksum_add(length, buf);         // payload
-    sum += net_checksum_add(8, addrs);            // src + dst address
-    sum += proto + length;                        // protocol & length
+    sum += net_checksum_add(length, buf);         /* payload */
+    sum += net_checksum_add(addr_len << 1, addrs);/* src + dst address */
+    sum += proto + length;                        /* protocol & length */
     return net_checksum_finish(sum);
+}
+
+/* Takes the base address to avoid unaligned address accessing */
+static bool is_ipv4_fragment_by_base_address(uint8_t *base)
+{
+    return (be16_to_cpu((ldub_p(base + offsetof(struct ip_header, ip_off))) & \
+            (IP_OFFMASK | IP_MF)) != 0);
+}
+
+static void net_checksum_ipv4(uint8_t *ip, int csum_flag)
+{
+    uint16_t csum;
+
+    /* Calculate IP checksum */
+    if (csum_flag & CSUM_IP) {
+        stw_he_p(ip + offsetof(struct ip_header, ip_sum), 0);
+        csum = net_raw_checksum((uint8_t *)ip, IP_HDR_GET_LEN(ip));
+        stw_he_p(ip + offsetof(struct ip_header, ip_sum), csum);
+    }
+
+    if (is_ipv4_fragment_by_base_address(ip)) {
+        qemu_log_mask(LOG_GUEST_ERROR,
+            "%s: fragmented IP packet!", __func__);
+    }
+}
+
+/* returns payload length. */
+static uint16_t net_payload_length_ipv4(uint8_t *ip)
+{
+    return lduw_be_p(ip + offsetof(struct ip_header, ip_len)) - \
+                     sizeof(struct ip_header);
+}
+
+/* returns payload length. */
+static uint16_t net_payload_length_ipv6(uint8_t *ip)
+{
+    return lduw_be_p(ip + offsetof(struct ip6_header, ip6_plen));
 }
 
 void net_checksum_calculate(void *data, int length, int csum_flag)
 {
-    int mac_hdr_len, ip_len;
-    struct ip_header *ip;
+    int mac_hdr_len, ip_version;
+    uint16_t ip_len;
     uint16_t csum;
+    uint8_t ip_p, addr_len;
+    uint8_t *ip_src;
+    uint8_t *ip_nxt;
+    uint8_t *ip_base_addr;
 
     /*
      * Note: We cannot assume "data" is aligned, so the all code uses
      * some macros that take care of possible unaligned access for
      * struct members (just in case).
+     * We can't access the member directly neither. Thus the access wll be
+     * in the format of base_address + offsetof(struct, member).
      */
 
     /* Ensure we have at least an Eth header */
@@ -101,54 +147,54 @@ void net_checksum_calculate(void *data, int length, int csum_flag)
         return;
     }
 
-    ip = (struct ip_header *)((uint8_t *)data + mac_hdr_len);
+    ip_base_addr = data + mac_hdr_len;
+    ip_version = (ldub_p(ip_base_addr + offsetof(struct ip_header, ip_ver_len)) \
+                 >> 4) & 0xf;
 
-    if (IP_HEADER_VERSION(ip) != IP_HEADER_VERSION_4) {
-        return; /* not IPv4 */
-    }
-
-    /* Calculate IP checksum */
-    if (csum_flag & CSUM_IP) {
-        stw_he_p(&ip->ip_sum, 0);
-        csum = net_raw_checksum((uint8_t *)ip, IP_HDR_GET_LEN(ip));
-        stw_be_p(&ip->ip_sum, csum);
-    }
-
-    if (IP4_IS_FRAGMENT(ip)) {
-        return; /* a fragmented IP packet */
-    }
-
-    ip_len = lduw_be_p(&ip->ip_len);
-
-    /* Last, check that we have enough data for the all IP frame */
-    if (length < ip_len) {
+    switch (ip_version) {
+    case IP_HEADER_VERSION_4:
+        net_checksum_ipv4(ip_base_addr, csum_flag);
+        ip_len = net_payload_length_ipv4(ip_base_addr);
+        ip_p = ldub_p(ip_base_addr + offsetof(struct ip_header, ip_p));
+        ip_src = ip_base_addr + offsetof(struct ip_header, ip_src);
+        ip_nxt = ip_base_addr + sizeof(struct ip_header);
+        addr_len = 4;
+        break;
+    case IP_HEADER_VERSION_6:
+        ip_len = net_payload_length_ipv6(ip_base_addr);
+        ip_p = ldub_p(ip_base_addr + offsetof(struct ip6_header, ip6_nxt));
+        ip_src = ip_base_addr + offsetof(struct ip6_header, ip6_src);
+        ip_nxt = ip_base_addr + sizeof(struct ip6_header);
+        addr_len = 16;
+        break;
+    default:
+        qemu_log_mask(LOG_GUEST_ERROR,
+            "%s: Unknown IP version: %d", __func__, ip_version);
         return;
     }
 
-    ip_len -= IP_HDR_GET_LEN(ip);
-
-    switch (ip->ip_p) {
+    switch (ip_p) {
     case IP_PROTO_TCP:
     {
         if (!(csum_flag & CSUM_TCP)) {
             return;
         }
 
-        tcp_header *tcp = (tcp_header *)(ip + 1);
+        /* ip_nxt is basically the offset of tcp_header */
+        uint8_t *tcp_base_addr = ip_nxt;
 
         if (ip_len < sizeof(tcp_header)) {
             return;
         }
 
         /* Set csum to 0 */
-        stw_he_p(&tcp->th_sum, 0);
+        stw_he_p(tcp_base_addr + offsetof(tcp_header, th_sum), 0);
 
-        csum = net_checksum_tcpudp(ip_len, ip->ip_p,
-                                   (uint8_t *)&ip->ip_src,
-                                   (uint8_t *)tcp);
+        csum = net_checksum_tcpudp(ip_len, ip_p, ip_src,
+                                   tcp_base_addr, addr_len);
 
         /* Store computed csum */
-        stw_be_p(&tcp->th_sum, csum);
+        stw_be_p(tcp_base_addr + offsetof(tcp_header, th_sum), csum);
 
         break;
     }
@@ -158,21 +204,21 @@ void net_checksum_calculate(void *data, int length, int csum_flag)
             return;
         }
 
-        udp_header *udp = (udp_header *)(ip + 1);
+        /* ip_nxt is the offset of udp_header */
+        uint8_t *udp_base_addr = ip_nxt;
 
         if (ip_len < sizeof(udp_header)) {
             return;
         }
 
         /* Set csum to 0 */
-        stw_he_p(&udp->uh_sum, 0);
+        stw_he_p(udp_base_addr + offsetof(udp_header, uh_sum), 0);
 
-        csum = net_checksum_tcpudp(ip_len, ip->ip_p,
-                                   (uint8_t *)&ip->ip_src,
-                                   (uint8_t *)udp);
+        csum = net_checksum_tcpudp(ip_len, ip_p, ip_src,
+                                   udp_base_addr, addr_len);
 
         /* Store computed csum */
-        stw_be_p(&udp->uh_sum, csum);
+        stw_be_p(udp_base_addr + offsetof(udp_header, uh_sum), csum);
 
         break;
     }
