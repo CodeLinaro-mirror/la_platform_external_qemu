738a574da3c3859bf6eae97c2b72c0186371643d
Titus Rwantare <titusr@google.com>
2024-04-29 17:20:09 +0000

    hw/arm: attach eSPI device model to Nuvoton 8xx
    Google-Bug-Id: 335691260
    Signed-off-by: Titus Rwantare <titusr@google.com>
    Change-Id: Ife07d71f2b2300afbf3fb131621260a9fe51117a

--- include/hw/arm/npcm8xx.h
+++ include/hw/arm/npcm8xx.h
@@ -37,6 +37,7 @@
 #include "hw/sd/npcm7xx_sdhci.h"
 #include "hw/timer/npcm7xx_timer.h"
 #include "hw/ssi/npcm7xx_fiu.h"
+#include "hw/ssi/npcm_espi.h"
 #include "hw/usb/hcd-ehci.h"
 #include "hw/usb/hcd-ohci.h"
 #include "target/arm/cpu.h"
@@ -108,6 +109,7 @@ struct NPCM8xxState {
     NPCMPCIERCState     pcierc;
     SVCI3C              i3c[6];
     NPCM8xxGDMA         gdma[3];
+    NPCMESPIState       espi;
 };
 
 struct NPCM8xxClass {
