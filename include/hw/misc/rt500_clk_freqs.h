4b665e22ebdb2f1b762e655f06bab4af90afb82d
Octavian Purdila <tavip@google.com>
2024-08-02 17:20:29 -0700

    hw/misc: add support for RT500's clock controller
    It supports system and audio PLL initialization and SYSTICK and
    OSTIMER clock source selection.
    
    The patch includes automatically generated headers which contains the
    register layout and helpers.
    
    The headers can be regenerated with the svd-rt500-clkctl0 and
    svd-rt500-clkctl1 targets when the build is configured with
    --enable-mcux-soc-svd.
    
    GitWatcher: ignore
    Change-Id: Ice11c48d29d91709e5325b3aed2417805c44d9b1
    Signed-off-by: Octavian Purdila <tavip@google.com>

---
+++ include/hw/misc/rt500_clk_freqs.h
@@ -0,0 +1,15 @@
+/*
+ * QEMU model for RT500 Clock Controller
+ *
+ * Copyright (c) 2024 Google LLC
+ *
+ * SPDX-License-Identifier: GPL-2.0-or-later
+ */
+
+#ifndef HW_MISC_RT500_CLK_FREQS_H
+#define HW_MISC_RT500_CLK_FREQS_H
+
+#define RTC32KHZ_CLK_HZ 32000
+#define LPOSC_CLK_HZ 1000000
+
+#endif /* HW_MISC_RT500_CLK_FREQS_H */
