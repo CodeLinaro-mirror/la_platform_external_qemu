CodeLinaro-mirror/la_platform_external_qemu

commit 766e9306e32e89ba35d9059fc924fe0793f8291e

fix: Initialize metrics early
This adds support for initializing metrics early on and enables the
reporting of compatibility failures. Failure metrics are reported and
flushed before continuing.

Metrics will not be enabled when running under fuchsia.

Test: ANDROID_EMU_ABORT_SYSTEM_CHECKS=1 ./objs/emulator @31L -debug-metrics -metrics-to-console
event time 1741137663375 ms
{ kind: EMULATOR_PING studio_session_id: "7af11147-964c-4b07-a8e0-e3306c52f3ea" product_details { product: EMULATOR build: "35.5.4-standalone-0" version: "35.5.4.0" } emulator_details { system_time: 60 user_time: 240 wall_time: 245 core_version: "2.12.0" emu_compat_info { status: AVD_COMPATIBILITY_STATUS_ERROR check: AVD_COMPATIBILITY_CHECK_SYSTEM_ABORT } } }
FATAL        | The user forced a compatibility error, unset ANDROID_EMU_ABORT_SYSTEM_CHECKS environment variable to launch the emulator.

Bug: 400789061
Bug: 390477464
Change-Id: I52e7783df3036abd976d7f7007611e49fab7918f

android-qemu2-glue/main.cpp
@@ -16,10 +16,13 @@
 #include "aemu/base/memory/ScopedPtr.h"
 #include "aemu/base/system/Win32Utils.h"
 #include "aemu/base/threads/Async.h"
+#include "aemu/base/Uuid.h"
 #include "aemu/base/threads/Thread.h"
 #include "android-qemu2-glue/base/files/KernelCmdLineLoader.h"
 #include "android-qemu2-glue/qemu-console-factory.h"
 #include "android/android.h"
+#include "android/metrics/MetricsReporter.h"
+#include "android/metrics/PeriodicReporter.h"
 #include "android/avd/BugreportInfo.h"
 #include "android/base/files/IniFile.h"
 #include "android/base/system/System.h"
@@ -344,18 +347,6 @@ static int genHwIniFile(AndroidHwConfig* hw, const char* coreHwIniPath) {
     }
 
     return 0;
-}
-
-extern "C" void android_report_compatibility_checks() {
-    // Reload the compatibility checks and report them.
-    bool reportMetrics = !getConsoleAgents()->settings->is_fuchsia() &&
-                         !getConsoleAgents()->settings->android_qemu_mode();
-
-    if (reportMetrics) {
-        auto results = AvdCompatibilityManager::instance().check(
-                getConsoleAgents()->settings->avdInfo());
-        AvdCompatibilityManager::instance().reportMetrics(results);
-    }
 }
 
 static void updateDataSystemSubdirectory(const char* dataDirectory,
@@ -1812,9 +1803,21 @@ extern "C" int main(int argc, char** argv) {
     opts->ranchu = 1;
 
     getConsoleAgents()->settings->inject_AvdInfo(avd);
+    bool reportMetrics = !opts->fuchsia;
 
-    // Check compatibility and exit in case of failure..
-    AvdCompatibilityManager::ensureAvdCompatibility(avd);
+    if (reportMetrics) {
+        auto sessionId = android::base::Uuid::generate().toString();
+        android::metrics::MetricsReporter::start(
+                sessionId, EMULATOR_VERSION_STRING,
+                EMULATOR_FULL_VERSION_STRING, QEMU_VERSION);
+        android::metrics::PeriodicReporter::start(
+                &android::metrics::MetricsReporter::get(),
+                android::base::Looper::create());
+    }
+
+    // Check compatibility and exit in case of failure, reporting metrics if
+    // needed
+    AvdCompatibilityManager::ensureAvdCompatibility(avd, reportMetrics);
 
     if (opts->read_only) {
         android::base::disableRestart();

android/android-emu/android/metrics/DependentMetrics.cpp
@@ -910,9 +910,6 @@ bool android_metrics_start(const char* emulatorVersion,
                            const char* emulatorFullVersion,
                            const char* qemuVersion,
                            int controlConsolePort) {
-    auto sessionId = android::base::Uuid::generate().toString();
-    MetricsReporter::start(sessionId,
-                           emulatorVersion, emulatorFullVersion, qemuVersion);
     PeriodicReporter::start(&MetricsReporter::get(),
                             android::base::ThreadLooper::get());
 
@@ -928,6 +925,7 @@ bool android_metrics_start(const char* emulatorVersion,
                 // there's nothing to do here.
                 return true;
             });
+    auto sessionId = MetricsReporter::get().sessionId();
     // Collect PerfStats metrics every 5 seconds.
     auto perfStatReporter = android::metrics::PerfStatReporter::create(
             sessionId, android::base::ThreadLooper::get(), 5 * 1000);

android/emu/check/include/android/emulation/compatibility_check.h
@@ -192,9 +192,12 @@ public:
      *
      * @param avd A pointer to the AvdInfo struct representing the AVD to be
      * checked.
+     * @param reportMetrics A boolean indicating whether to report metrics for
+     * the compatibility checks. If true, metrics will be reported for warnings
+     * and errors. If false, no metrics will be reported. Defaults to false.
      */
-
-    static void ensureAvdCompatibility(AvdInfo* avd);
+    static void ensureAvdCompatibility(AvdInfo* avd,
+                                       bool reportMetrics = false);
 
 private:
     AvdCompatibilityManager() = default;

android/emu/check/src/android/compatibility_check.cpp
@@ -82,10 +82,12 @@ void AvdCompatibilityManager::reportMetrics(
         if (result.status != AvdCompatibility::Ok) {
             MetricsReporter::get().report(
                     [result](android_studio::AndroidStudioEvent* event) {
+
                         auto info = event->mutable_emulator_details()
                                             ->mutable_emu_compat_info();
                         info->CopyFrom(result.metrics);
                     });
+            MetricsReporter::get().finishPendingReports();
         }
     }
 }
@@ -100,9 +102,13 @@ bool AvdCompatibilityManager::hasCompatibilityErrors(
     return false;  // No errors were found in any of the results
 }
 
-void AvdCompatibilityManager::ensureAvdCompatibility(AvdInfo* avd) {
+void AvdCompatibilityManager::ensureAvdCompatibility(AvdInfo* avd, bool reportMetrics) {
     auto acm = AvdCompatibilityManager::instance();
     auto results = acm.check(avd);
+
+    if (reportMetrics) {
+        acm.reportMetrics(results);
+    }
 
     // Prints the results for android studio.
     if (acm.hasCompatibilityErrors(results)) {

vl.c
@@ -3339,8 +3339,6 @@ static void android_teardown_metrics()
 }
 
 
-extern void android_report_compatibility_checks();
-
 static const char openglInitFailureMessage[] =
     "OpenGLES emulation failed to initialize. "
     "Please consider the following troubleshooting steps:\n\n"
@@ -3355,7 +3353,6 @@ static const char openglInitFailureMessage[] =
 static bool android_reporting_setup(void)
 {
     android_init_metrics();
-    android_report_compatibility_checks();
     registerCrashInfo();
 
     if (!is_opengl_alive) {

commit 0e2bcdc54a1259aada3d5b67bb11fe0b6a4072e7

fix: Prevent crash during metrics initialization
Commit 766e9306e3 introduced early metrics initialization, leading to
the creation of a custom thread looper. This looper could later be
overwritten, causing crashes.

This change updates the metrics initializer to accept a custom looper as
a parameter, ensuring metrics do not run on an existing thread looper
and preventing accidental overwrites.

Bug: 400789061
Bug: 390477464
Change-Id: I12e049fbeaadbb4a77cedbc75591b8ba49ceb7bf

android-qemu2-glue/main.cpp
@@ -1806,13 +1806,13 @@ extern "C" int main(int argc, char** argv) {
     bool reportMetrics = !opts->fuchsia;
 
     if (reportMetrics) {
+        auto looper = android::base::Looper::create();
         auto sessionId = android::base::Uuid::generate().toString();
         android::metrics::MetricsReporter::start(
                 sessionId, EMULATOR_VERSION_STRING,
-                EMULATOR_FULL_VERSION_STRING, QEMU_VERSION);
+                EMULATOR_FULL_VERSION_STRING, QEMU_VERSION, looper);
         android::metrics::PeriodicReporter::start(
-                &android::metrics::MetricsReporter::get(),
-                android::base::Looper::create());
+                &android::metrics::MetricsReporter::get(), looper);
     }
 
     // Check compatibility and exit in case of failure, reporting metrics if

android/emu/metrics/include/android/metrics/MetricsReporter.h
@@ -18,7 +18,7 @@
 #include <vector>      // for vector
 
 #include "aemu/base/Compiler.h"              // for DISALLOW_COPY_ASSIGN_...
-
+#include "aemu/base/async/ThreadLooper.h"
 #include "aemu/base/synchronization/Lock.h"  // for Lock
 #include "android/base/system/System.h"         // for System, System::Duration
 #include "android/metrics/MetricsWriter.h"      // for MetricsWriter, Metric...
@@ -95,7 +95,8 @@ public:
     static void start(const std::string& sessionId,
                       std::string_view emulatorVersion,
                       std::string_view emulatorFullVersion,
-                      std::string_view qemuVersion);
+                      std::string_view qemuVersion,
+                      android::base::Looper* looper = base::ThreadLooper::get());
     static void stop(MetricsStopReason reason);
     static MetricsReporter& get();
 

android/emu/metrics/src/android/metrics/MetricsReporter.cpp
@@ -92,7 +92,8 @@ void warnAboutNoMetricsConsentInput();
 void MetricsReporter::start(const std::string& sessionId,
                             std::string_view emulatorVersion,
                             std::string_view emulatorFullVersion,
-                            std::string_view qemuVersion) {
+                            std::string_view qemuVersion,
+                            base::Looper* looper) {
     MetricsWriter::Ptr writer;
     if (getConsoleAgents()
             ->settings->android_cmdLineOptions()->metrics_to_console) {
@@ -138,7 +139,7 @@ void MetricsReporter::start(const std::string& sessionId,
         writer = FileMetricsWriter::create(
                 sessionId, getSpoolDirectory(),
                 1000,  // record limit per single file
-                base::ThreadLooper::get(),
+                looper,
                 static_cast<long>(10 * 60 *
                                   1000));  // time limit for a single file, ms
     }