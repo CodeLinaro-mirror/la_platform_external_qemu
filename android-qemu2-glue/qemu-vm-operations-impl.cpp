e6e01b9baa7cae1860b131ac6e5c2948b629a072
Bo Hu <bohu@google.com>
2025-03-27 11:09:36 -0700

    Revert "run unmap_user_backed_ram on main loop"
    This reverts commit dfa3d03e75f6e9ee638141576a014857474431b0.
    
    Bug: 406768299
    Bug: 405345461
    
    Change-Id: I16e0b152d14dcfa62b986ca2c30f3e8e0b55b1f4

--- android-qemu2-glue/qemu-vm-operations-impl.cpp
+++ android-qemu2-glue/qemu-vm-operations-impl.cpp
@@ -29,7 +29,6 @@ extern "C" {
 
 #include "aemu/base/files/PathUtils.h"             // for pj, PathUtils
 #include "android/base/system/System.h"               // for System
-#include "aemu/base/async/ThreadLooper.h"
 #include "android/emulation/CpuAccelerator.h"         // for GetCurrentCpuAc...
 #include "host-common/HostmemIdMapping.h"       // for android_emulati...
 #include "host-common/VmLock.h"                 // for RecursiveScoped...
@@ -961,10 +960,11 @@ static void map_user_backed_ram(uint64_t gpa, void* hva, uint64_t size) {
                               USER_BACKED_RAM_FLAGS_READ | USER_BACKED_RAM_FLAGS_WRITE);
 }
 
-static void unmap_user_backed_ram(std::unordered_map<uint64_t, MemoryMapping>& aMemoryMap, uint64_t gpa, uint64_t size) {
+static void unmap_user_backed_ram(uint64_t gpa, uint64_t size) {
+    android::RecursiveScopedVmLock vmlock;
     DMEM("%s: gpa:0x%lx size:0x%lx", __func__, gpa, size);
-    auto res = aMemoryMap.find(gpa);
-    if (res == aMemoryMap.end()) {
+    auto res = sMemoryMap.find(gpa);
+    if (res == sMemoryMap.end()) {
         derror("Tried to unmap non-existant memory region {gpa:0x%lx size:0x%lx}. Ignoring.",
              gpa, size);
         return;
@@ -974,14 +974,8 @@ static void unmap_user_backed_ram(std::unordered_map<uint64_t, MemoryMapping>& a
                "Ignoring.", gpa, size, res->first, res->second.hva, res->second.memory_size);
         return;
     }
-    aMemoryMap.erase(res);
+    sMemoryMap.erase(res);
     qemu_user_backed_ram_unmap(gpa, size);
-}
-
-static void unmap_user_backed_ram_on_mainloop(uint64_t gpa, uint64_t size) {
-    android::base::ThreadLooper::runOnMainLooper(
-                [gpa=gpa, size=size] { unmap_user_backed_ram(sMemoryMap, gpa, size); }
-                );
 }
 
 static void get_vm_config(VmConfiguration* out) {
@@ -1288,7 +1282,7 @@ static const QAndroidVmOperations sQAndroidVmOperations = {
         .setSnapshotCallbacks = set_snapshot_callbacks,
         .setSnapshotProtobuf = set_snapshot_protobuf,
         .mapUserBackedRam = map_user_backed_ram,
-        .unmapUserBackedRam = unmap_user_backed_ram_on_mainloop,
+        .unmapUserBackedRam = unmap_user_backed_ram,
         .getVmConfiguration = get_vm_config,
         .setFailureReason = set_failure_reason,
         .setExiting = set_exiting,
