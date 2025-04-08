80af8ca5c5d6aea84710f2e4c450507bf7883dae
Joe Komlodi <komlodi@google.com>
2025-04-08 22:39:15 +0000

    hw/i3c/mipi-hci: Add command and response descriptors
    These are used in both DMA and PIO transactions, so they're factored into
    their own file, even though we're only implementing DMA transactions for now.
    
    GitWatcher: ignore
    Google-Bug-Id: 406094038
    Change-Id: I8e304919f50a36f7a61d433d4584eacd0851cf10
    Signed-off-by: Joe Komlodi <komlodi@google.com>

---
+++ hw/i3c/hci-cmd.h
@@ -0,0 +1,115 @@
+/*
+ * MIPI HCI I3C controller commands
+ *
+ * Copyright (C) 2025 Google, LLC
+ *
+ * SPDX-License-Identifier: GPL-2.0-or-later
+ */
+
+#ifndef HCI_CMD_H
+#define HCI_CMD_H
+
+#include "hw/i3c/mipi-hci.h"
+
+typedef struct AddrCmd {
+  uint8_t cmd_attr:3;
+  uint8_t tid:4; /* Transaction ID. */
+  uint16_t cmd:8;
+  uint8_t rsvd:1;
+  uint8_t dev_index:7;
+  uint16_t rsvd2:3;
+  uint8_t dev_count:4;
+  uint8_t roc:1; /* Response on completion. */
+  uint8_t toc:1; /* Terminate on completion (STOP). */
+  uint32_t rsvd3;
+} __attribute__((packed)) AddrCmd;
+QEMU_BUILD_BUG_ON(sizeof(AddrCmd) != sizeof(uint64_t));
+
+typedef struct ImmediateXfer {
+  uint8_t cmd_attr:3;
+  uint8_t tid:4; /* Transaction ID. */
+  uint16_t cmd:8;
+  uint8_t cp:1; /* Command (CCC or HDR) present. */
+  uint8_t dev_index:7;
+  uint16_t dtt:3; /* Data transfer type. */
+  uint8_t mode:3;
+  uint8_t rnw:1;
+  uint8_t roc:1; /* Response on completion. */
+  uint8_t toc:1; /* Terminate on completion (STOP). */
+  uint8_t data[4];
+} __attribute__((packed)) ImmediateXfer;
+QEMU_BUILD_BUG_ON(sizeof(ImmediateXfer) != sizeof(uint64_t));
+
+typedef struct RegularXfer {
+  uint8_t cmd_attr:3;
+  uint8_t tid:4; /* Transaction ID. */
+  uint16_t cmd:8;
+  uint8_t cp:1; /* Command (CCC or HDR) present. */
+  uint8_t dev_index:7;
+  uint8_t rsvd:1;
+  uint8_t sre:1; /* Short read is error. */
+  uint8_t dbp:1; /* Defining byte present. */
+  uint8_t mode:3;
+  uint8_t rnw:1;
+  uint8_t roc:1; /* Response on completion. */
+  uint8_t toc:1; /* Terminate on completion (STOP). */
+  uint8_t def_byte;
+  uint8_t rsvd2;
+  uint16_t data_length;
+} __attribute__((packed)) RegularXfer;
+QEMU_BUILD_BUG_ON(sizeof(RegularXfer) != sizeof(uint64_t));
+
+typedef struct ComboXfer {
+  uint8_t cmd_attr:3;
+  uint8_t tid:4; /* Transaction ID. */
+  uint16_t cmd:8;
+  uint8_t cp:1; /* Command (CCC or HDR) present. */
+  uint8_t dev_index:5;
+  uint8_t rsvd:1;
+  uint8_t dlp:2; /* Data length present. */
+  uint8_t fpm:1; /* First phase mode. */
+  uint8_t sub_offset_16:1;
+  uint8_t mode:3;
+  uint8_t rnw:1;
+  uint8_t roc:1; /* Response on completion. */
+  uint8_t toc:1; /* Terminate on completion (STOP). */
+  uint16_t offset;
+  uint16_t data_length;
+} __attribute__((packed)) ComboXfer;
+QEMU_BUILD_BUG_ON(sizeof(ComboXfer) != sizeof(uint64_t));
+
+typedef struct InternalControl {
+  uint8_t cmd_attr:3;
+  uint8_t tid:4; /* Transaction ID. */
+  uint8_t vip:1; /* Vendor info present. */
+  uint32_t mipi_rsvd:22;
+  uint32_t rsvd;
+} __attribute__((packed)) InternalControl;
+QEMU_BUILD_BUG_ON(sizeof(InternalControl) != sizeof(uint64_t));
+
+typedef union CmdDescr {
+  AddrCmd addr_cmd;
+  ImmediateXfer immediate_xfer;
+  RegularXfer regular_xfer;
+  ComboXfer combo_xfer;
+  InternalControl internal_control;
+
+  uint8_t cmd_attr:3;
+  uint32_t val32[2];
+  uint64_t val64;
+} __attribute__((packed)) CmdDescr;
+QEMU_BUILD_BUG_ON(sizeof(CmdDescr) != sizeof(uint64_t));
+
+typedef union RespDescr {
+    struct {
+        uint16_t length;
+        uint8_t rsvd;
+        uint8_t tid:4; /* Transaction ID. */
+        uint8_t err:4;
+    } __attribute__((packed)) resp;
+
+    uint32_t val32;
+} __attribute__((packed)) RespDescr;
+QEMU_BUILD_BUG_ON(sizeof(RespDescr) != sizeof(uint32_t));
+
+#endif  /* HCI_CMD_H */
