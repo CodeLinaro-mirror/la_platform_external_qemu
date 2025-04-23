7b62a1577db4d793b1008af37bacb0aa2a7ed5e2
Nabih Estefan <nabihestefan@google.com>
2025-04-23 22:22:51 +0000

    hw/arm/npcm8xx: Add PECI Support
    Splitting off the device specific parts of the original npcm8xx soc
    patch
    
    GitWatcher: ignore
    Change-Id: I92f766e2963a4fccb35d3bd5142d4fd7eeadd40d
    Signed-off-by: Nabih Estefan <nabihestefan@google.com>
    Signed-off-by: Hao Wu <wuhaotsh@google.com>

--- include/hw/arm/npcm8xx.h
+++ include/hw/arm/npcm8xx.h
@@ -36,6 +36,7 @@
 #include "hw/net/npcm_pcs.h"
 #include "hw/nvram/npcm7xx_otp.h"
 #include "hw/pci-host/npcm_pcierc.h"
+#include "hw/peci/npcm7xx_peci.h"
 #include "hw/sd/npcm7xx_sdhci.h"
 #include "hw/timer/npcm7xx_timer.h"
 #include "hw/ssi/npcm7xx_fiu.h"
@@ -117,6 +118,7 @@ struct NPCM8xxState {
     NPCM7xxPCIMBoxState pci_mbox[2];
     NPCM7xxSDHCIState   mmc;
     NPCMPSPIState       pspi;
+    NPCM7xxPECIState    peci;
     NPCMPCIERCState     pcierc;
     NPCMESPIState       espi;
     NPCM8xxGDMA         gdma[3];
