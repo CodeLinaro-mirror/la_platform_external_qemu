96836055bae14c8de6263f3217eb5b18c3e804d5
Bo Hu <bohu@google.com>
2025-05-05 14:59:40 -0700

    Turn XDG_RUNTIME_DIR warning into debug print
    It is not an issue when this is un-defined.
    
    Change-Id: I0298f508799abf56e8da2e2c1f4a02f19eb97815

--- android/emulator/main-emulator.cpp
+++ android/emulator/main-emulator.cpp
@@ -490,7 +490,7 @@ int main(int argc, char** argv) {
             }
         }
 #endif
-        dwarning("XDG_RUNTIME_DIR is not defined, default to %s",
+        dprint("XDG_RUNTIME_DIR is not defined, default to %s",
                  default_runtime_dir);
         System::get()->envSet(kXDG_RUNTIME_DIR_NAME, "/tmp");
     }
