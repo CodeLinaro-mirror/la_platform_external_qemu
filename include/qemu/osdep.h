d0660daf573c08713e538faf562c336b477074fe
Titus Rwantare <titusr@google.com>
2025-05-14 21:44:25 +0000

    osdep: provide DIV_ROUND_CLOSEST macro
    Lifted from linux/include/linux/math.h.
    This allows QEMU sensor models to align lossy integer division with linux.
    
    GitWatcher: ignore
    Google-Bug-Id: 416632956
    Signed-off-by: Titus Rwantare <titusr@google.com>
    Change-Id: Ibac47229c47e755f58158b8af003f3674177fe4c

--- include/qemu/osdep.h
+++ include/qemu/osdep.h
@@ -513,6 +513,24 @@ void QEMU_ERROR("code path is reachable")
 #endif
 
 /*
+ * Divide positive or negative dividend by positive divisor and round
+ * to closest integer. Result is undefined for negative divisors and
+ * for negative dividends if the divisor variable type is unsigned.
+ */
+#ifndef DIV_ROUND_CLOSEST
+#define DIV_ROUND_CLOSEST(x, divisor)(          \
+{                                               \
+    typeof(x) __x = x;                          \
+    typeof(divisor) __d = divisor;              \
+    (((typeof(x))-1) > 0 ||                     \
+     ((typeof(divisor))-1) > 0 || (__x) > 0) ?  \
+        (((__x) + ((__d) / 2)) / (__d)) :       \
+        (((__x) - ((__d) / 2)) / (__d));        \
+}                                               \
+)
+#endif
+
+/*
  * &(x)[0] is always a pointer - if it's same type as x then the argument is a
  * pointer, not an array.
  */
