d90cb5155eb3f36a8462aa04eedc5ee6308b3cff
Serdar Kocdemir <kocdemir@google.com>
2025-07-24 10:38:52 +0100

    Remove ANDROID_EMUGL_FIXED_BACKEND_LIST
    In order to reduce the complexity around how we setup our renderer
    and avoid issues on user devices where environment variables can
    cause untested code paths to be taken.
    
    Bug: 407477551
    Test: EmuglConfig
    Change-Id: Ie0c53e041ff6d88c87f9f1eeb4e61634f0a6994a

--- android/android-ui/modules/aemu-gl-init/src/android/opengl/emugl_config.cpp
+++ android/android-ui/modules/aemu-gl-init/src/android/opengl/emugl_config.cpp
@@ -74,16 +74,8 @@ static EmuglBackendList* sBackendList = NULL;
 
 static void resetBackendList() {
     delete sBackendList;
-    if (System::getEnvironmentVariable("ANDROID_EMUGL_FIXED_BACKEND_LIST") == "1") {
-        std::vector<std::string> fixedBackendNames = {
-            "swiftshader_indirect",
-            "angle_indirect",
-        };
-        sBackendList = new EmuglBackendList(fixedBackendNames);
-    } else {
-        sBackendList = new EmuglBackendList(
-                System::get()->getLauncherDirectory().c_str());
-    }
+    sBackendList = new EmuglBackendList(
+            System::get()->getLauncherDirectory().c_str());
 }
 
 static bool stringVectorContains(const std::vector<std::string>& list,
