eda10404671fae7aeec7eae299dd858786d73814
Serdar Kocdemir <kocdemir@google.com>
2026-02-18 15:47:37 +0000

    Use vulkan version 1.1 for gpu capability checks
    When created with vulkan 1.0, some features won't be reported as
    supported on some devices. We already upgrade to 1.1 when supported
    so, better to make the checks with that api level.
    
    Bug: 485540153
    Test: XR emulator on AMD
    Change-Id: I807a321033aa18f84f9392291340b5dfd09c428c

--- android/android-ui/modules/aemu-gl-init/src/android/opengl/emugl_config.cpp
+++ android/android-ui/modules/aemu-gl-init/src/android/opengl/emugl_config.cpp
@@ -744,10 +744,10 @@ bool emuglConfig_get_vulkan_hardware_gpu_support_info(
     VkApplicationInfo appInfo{};
     appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
     appInfo.pApplicationName = "DetectGpuInfo";
-    appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+    appInfo.applicationVersion = VK_MAKE_VERSION(1, 1, 0);
     appInfo.pEngineName = "test_engine";
-    appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
-    appInfo.apiVersion = VK_API_VERSION_1_0;
+    appInfo.engineVersion = VK_MAKE_VERSION(1, 1, 0);
+    appInfo.apiVersion = VK_API_VERSION_1_1;
 
     VkInstanceCreateInfo createInfo{};
     createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
