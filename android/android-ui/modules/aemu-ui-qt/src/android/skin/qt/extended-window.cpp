d0d1732f9c1fb02cbb383ad6f57468fb89385cde
Daniel Rosenberg <drosen@google.com>
2026-03-12 20:03:53 -0700

    Remove obsolete microphone signals
    These functions were removed in favor of the listener based
    synchronization method.
    
    Bug: 492325171
    Test: Boot and check for "QObject::connect: No such signal" messages
    Change-Id: I57b9c6f9b75fa9936992d204356080c046006a5f

--- android/android-ui/modules/aemu-ui-qt/src/android/skin/qt/extended-window.cpp
+++ android/android-ui/modules/aemu-ui-qt/src/android/skin/qt/extended-window.cpp
@@ -157,11 +157,6 @@ ExtendedWindow::ExtendedWindow(EmulatorQtWindow* eW, ToolWindow* tW)
     connect(mToolWindow, SIGNAL(haveClipboardSharingKnown(bool)),
             mExtendedUi->settingsPage, SLOT(setHaveClipboardSharing(bool)));
 
-    connect(mToolWindow, SIGNAL(microphoneEnabledChanged()),
-            mExtendedUi->microphonePage, SLOT(onMicrophoneEnabledChanged()));
-    connect(mExtendedUi->microphonePage, SIGNAL(microphoneEnabledChanged()),
-            mToolWindow, SLOT(onMicrophoneEnabledChanged()));
-
     connect(mExtendedUi->recordAndPlaybackPage,
             SIGNAL(ensureVirtualSceneWindowCreated()), mToolWindow,
             SLOT(ensureVirtualSceneWindowCreated()));
