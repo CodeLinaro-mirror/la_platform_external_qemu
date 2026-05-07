aosp-mirror/platform_external_qemu

sysemu/os-win32.h: Windows clang-cl compiler fixes
This contains a series of small compiler fixes to enable the compilation
of qemu using clang-cl.

It mainly involves adding some missing header files and updating #ifdefs
to handle clang-cl specific things.

Signed-off-by: Erwin Jansen <jansene@google.com>
Change-Id: Ie59f04be6cc8e5b4d9a373e6864bb4196799bc04

include/sysemu/dma.h
@@ -14,6 +14,8 @@
 #include "exec/address-spaces.h"
 #include "block/block.h"
 #include "block/accounting.h"
+#include "block/aio.h"
+#include "qemu/iov.h"
 
 typedef enum {
     DMA_DIRECTION_TO_DEVICE = 0,

CodeLinaro-mirror/la_platform_external_qemu

(cherry picked from commit 4c18f4f8a06487570a92f5afc65dc7566f9b53f6)

include/system/dma.h
@@ -14,6 +14,8 @@
 #include "system/address-spaces.h"
 #include "block/block.h"
 #include "block/accounting.h"
+#include "qemu/aio.h"
+#include "qemu/iov.h"
 
 typedef enum {
     DMA_DIRECTION_TO_DEVICE = 0,